f7930ec41f34f80b5045ad6f97618b1de0fed50e
Kaushik-Rupapara-05 <127885277+Kaushik-Rupapara-05@users.noreply.github.com>
2023-04-27 23:43:20 +0530

    BTS sum Questation-2 in lab assignment3

---
+++ Node.java
@@ -0,0 +1,14 @@
+package com.lab_3.graded_project_question_2;
+
+public class Node {
+	public Node left;
+	public int data;
+	public Node right;
+	
+	public Node(int data)
+	{
+		this.left=null;
+		this.data=data;
+		this.right=null;
+	}
+}
